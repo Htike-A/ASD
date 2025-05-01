6d99a942f07d6449b8e76a4f80fa64e574e9e25c
Htike Aung <htike2.aung@live.uwe.ac.uk>
2025-05-01 04:50:29 +0100

    fixed pixels

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ from app import AppManager
 if __name__ == "__main__":
 	root = tk.Tk()
 	root.title("HC Cinemas")
-	root.geometry("800x500")
+	root.geometry("900x600")
 	app = AppManager(root)
 	root.mainloop()
